d834e3f1b54749cb764690db7be991ad77c701cc
Itachi Uchia <98242024+Andreshdez2022@users.noreply.github.com>
2022-06-25 17:15:22 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "d54fb55f472038b4dbd93eb85333234e"
 api_id = 15604626
-bot_token = "5384955022:AAFj46q6-gLT_NF0Kfq0YPi_NG9TPlq0DX4"
-bot_admin_group = -1001505005021
+bot_token = "5384955022:AAEKbPeoRUXkKdiu1ejSPoeW7pp-NtBRx94"
+bot_admin_group = -1791698696
